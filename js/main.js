49d3c4b1d56957e11b2d1c76c4b5447e4b82df42
dilettits <mantovani.d3@gmail.com>
2023-11-21 10:05:12 +0100

    indent

--- js/main.js
+++ js/main.js
@@ -10,9 +10,9 @@ createApp({
         eMail() {
             for (i = 0; i < 10; i++) {
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(response => {
-                console.log("Ricevuto: ", response.data);
-                let email = response.data.response;
-                this.emailList.push(email);
+                    console.log("Ricevuto: ", response.data);
+                    let email = response.data.response;
+                    this.emailList.push(email);
                 });
             }
         }
